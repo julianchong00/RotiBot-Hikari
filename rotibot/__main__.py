b035de9be1df346ee7333bb018f1ad9b965ca1d3
Julian Chong <julianchong00@outlook.com>
2022-05-27 18:33:28 +0100

    Update environment variable access and .gitignore

--- rotibot/__main__.py
+++ rotibot/__main__.py
@@ -4,23 +4,25 @@ import hikari
 import lightbulb
 import aiohttp
 
-from rotibot import GUILD_ID, STDOUT_CHANNEL_ID
+from dotenv import load_dotenv
+from pathlib import Path
 
-with open("./secrets/token") as f:
-    _token = f.read().strip()
+load_dotenv()
+env_path = Path("..") / ".env"
+load_dotenv(dotenv_path=env_path)
 
 bot = lightbulb.BotApp(
-    token=_token,
+    token=os.getenv("TOKEN"),
     prefix="!",
     intents=hikari.Intents.ALL,
-    default_enabled_guilds=GUILD_ID,
+    default_enabled_guilds=int(os.getenv("GUILD_ID")),
     help_slash_command=True,
 )
 
 
 @bot.listen()
 async def on_starting(event: hikari.StartingEvent) -> None:
-    channel = await bot.rest.fetch_channel(STDOUT_CHANNEL_ID)
+    channel = await bot.rest.fetch_channel(os.getenv("STDOUT_CHANNEL_ID"))
     await channel.send("Rotibot has been started!")
     bot.d.aio_session = aiohttp.ClientSession()
 
@@ -32,10 +34,6 @@ async def on_stopping(event: hikari.StoppingEvent) -> None:
 
 bot.load_extensions_from("./rotibot/extensions", must_exist=True)
 
-# @bot.listen(hikari.StartedEvent)
-# async def on_started(event: hikari.StartingEvent) -> None:
-#     channel = await bot.rest.fetch_channel(STDOUT_CHANNEL_ID)
-#     await channel.send("Hikari-Lightbulb bot has been started!")
 
 if __name__ == "__main__":
     if os.name != "nt":
