616674d4fa22ff95d4f1465d6914a7fd7e042bd1
Julian Chong <julianchong00@outlook.com>
2022-06-13 12:55:29 +0800

    Setup SQLAlchemy ORM with User model Implemented loading users from table into CSV file Updated requirements.txt with SQLAlchemy and psycopg2 dependencies

--- rotibot/database.py
+++ rotibot/database.py
@@ -1,12 +1,66 @@
 import os
+import typing as t
 from dotenv import load_dotenv
 from pathlib import Path
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import storage as store
 
 load_dotenv()
 env_path = Path("..") / ".env"
 load_dotenv(dotenv_path=env_path)
 
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_URI = os.getenv("DB_URI")
+
+"""
+PostgreSQL Database setup
+"""
+engine = create_engine(DB_URI)
+base = declarative_base()
+
+
+class User(base):
+    __tablename__ = "User"
+    discordID = Column(Integer, primary_key=True)
+    username = Column(String)
+    balance = Column(Integer)
+
+    def __repr__(self):
+        return "<User(discordID={}, username='{}', balance={})>".format(
+            self.discordID, self.username, self.balance
+        )
+
+
+"""
+Function to save all user data from CSV file into PostgreSQL database
+"""
+
+
+def saveAllUsers(users: t.Dict[int, t.Dict]) -> None:
+    Session = sessionmaker(engine)
+    session = Session()
+    return
+
+
+"""
+Function to load all user data from PostgreSQL database into CSV file
+"""
+
+
+def loadAllUsers() -> None:
+    Session = sessionmaker(engine)
+    session = Session()
+
+    users = session.query(User)
+
+    outerDict = dict()
+    for user in users:
+        innerDict = dict()
+        innerDict["username"] = user.username
+        innerDict["balance"] = int(user.balance)
+        outerDict[int(user.discordID)] = innerDict
+
+    store.write_csv(outerDict)
