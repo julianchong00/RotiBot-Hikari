39f64b8d63af96aff3953c4d0cca887d5112ddeb
Julian Chong <julianchong00@outlook.com>
2022-06-13 14:40:13 +0800

    CSV file should be created if it doesn't exist

--- rotibot/storage.py
+++ rotibot/storage.py
@@ -4,7 +4,7 @@ import typing as t
 # Reads users.csv and returns a Dictionary
 def read_csv() -> t.Dict[int, t.Dict]:
     users = dict()
-    with open("users.csv", "r") as file:
+    with open("users.csv", "r+") as file:
         csv_dict = csv.DictReader(file)
 
         for row in csv_dict:
@@ -18,7 +18,7 @@ def read_csv() -> t.Dict[int, t.Dict]:
 
 # Write from Dictionary into users.csv
 def write_csv(users: t.Dict[int, t.Dict]) -> None:
-    with open("users.csv", "w") as file:
+    with open("users.csv", "w+") as file:
         field_names = ["discordID", "username", "balance"]
         writer = csv.DictWriter(file, fieldnames=field_names)
 
